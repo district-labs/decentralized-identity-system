64812798ec3755082bbdf6ddd893039bdd0cba10
Kames <sudo@kames.me>
2023-08-02 11:11:05 -0400

    rename: identity-hub-test-server

--- apps/identity-hub-test-server/src/app.ts
+++ apps/identity-hub-test-server/src/app.ts
@@ -11,11 +11,11 @@ export const app = express();
 
   // DID Document Object
 const DID_ID = "did:dis:10:0x5FbDB2315678afecb367f032d93F642f64180aa3:0xF50C7Ce266d8F43cAF73a3307636E36C23090A7d"
+
 let DID = {
     '@context': 'https://www.w3.org/ns/did/v1',
-    id: DID_ID
+    id: "did:dis:10:0x5FbDB2315678afecb367f032d93F642f64180aa3:0xF50C7Ce266d8F43cAF73a3307636E36C23090A7d"
 }
-
 const signatureDID = "0xcc4d5855df6fae9b26d8fbdb18b0e8ecdb50c4bd7e13459ba7ad7fd2b500400c1befc3395bd7f0f6c6bbd129d0903924d2ad2d0809f1b42c0412bb5ede051cf01c"
 const signatureWallet = "0xab856af405d43f66dc003d335cec2889a4dfeb46b8dd2b9d9d1d4159f61b1ab32f4d5bb5bd51f05c9d1011b26d53408206fc2c4cf8adacd0d186ed125c5f430e1c"
 
